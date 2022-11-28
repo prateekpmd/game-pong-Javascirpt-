697b4678dc4400d0d0bafefce10d0e4a499c098f
prateekpmd <premanandprateek@gmail.com>
2022-11-28 18:24:23 +0530

    paddle and left paddle movement

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import Ball from "./Ball.js";
+import Paddle from "./Paddle.js";
 
 
 const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 
 
 let lastTime;
@@ -15,4 +18,9 @@ function update(time) {
 
  window.requestAnimationFrame(update);
 }
+
+
+document.addEventListener("mousemove", (e) => {
+  playerPaddle.position = (e.y / window.innerHeight) * 100;
+});
 window.requestAnimationFrame(update);

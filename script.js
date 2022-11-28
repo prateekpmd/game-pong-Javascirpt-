2f8e2f3b6a9ac0b7a09e70cdb27080c31dd0e3da
prateekpmd <premanandprateek@gmail.com>
2022-11-28 18:48:06 +0530

    background hsl

--- script.js
+++ script.js
@@ -16,6 +16,11 @@ function update(time) {
     console.log(delta);
     ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
     computerPaddle.update(delta, ball.y);
+     const hue = parseFloat(
+       getComputedStyle(document.documentElement).getPropertyValue("--hue")
+     );
+
+     document.documentElement.style.setProperty("--hue", hue + delta * 0.01);
     if (isLoose()) {
       handleLoose();
     }

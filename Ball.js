ddeafffebe70bf9448b163640b23c24510201db6
prateekpmd <premanandprateek@gmail.com>
2022-11-28 18:15:40 +0530

    update and reset functionality

---
+++ Ball.js
@@ -0,0 +1,61 @@
+const INITIAL_VELOCITY = 0.025;
+const VELOCITY_INCREASE = 0.00001;
+
+export default class Ball {
+  constructor(ballElement) {
+    this.ballElement = ballElement;
+    this.reset();
+  }
+  get x() {
+    return parseFloat(
+      getComputedStyle(this.ballElement).getPropertyValue("--x")
+    );
+  }
+
+  set x(value) {
+    this.ballElement.style.setProperty("--x", value);
+  }
+
+  get y() {
+    return parseFloat(
+      getComputedStyle(this.ballElement).getPropertyValue("--y")
+    );
+  }
+  set y(value) {
+    this.ballElement.style.setProperty("--y", value);
+  }
+  rect() {
+    return this.ballElement.getBoundingClientRect();
+  }
+
+  reset() {
+    this.x = 50;
+    this.y = 50;
+    this.direction = { x: 0, y: 0 };
+
+    const heading = randomNumberBetween(0, 2 * Math.PI);
+    this.direction = { x: Math.cos(heading), y: Math.sin(heading) };
+
+    console.log(this.direction);
+    this.velocity = INITIAL_VELOCITY;
+  }
+   update(delta) {
+    this.x += this.direction.x * this.velocity * delta;
+    this.y += this.direction.y * this.velocity * delta;
+
+    this.velocity += VELOCITY_INCREASE * delta;
+    const rect = this.rect();
+    if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+      this.direction.y *= -1;
+    }
+    if(rect.right>=window.innerWidth || rect.left<=0){
+          this.direction.x *= -1;
+    }
+}
+
+}
+
+
+function randomNumberBetween(min, max) {
+  return Math.random() * (max - min) + min;
+}
---
+++ script.js
@@ -0,0 +1,18 @@
+import Ball from "./Ball.js";
+
+
+const ball = new Ball(document.getElementById("ball"));
+
+
+let lastTime;
+function update(time) {
+     if (lastTime != null) {
+    const delta = time - lastTime;
+    console.log(delta);
+    ball.update(delta);
+     }
+ lastTime = time;
+
+ window.requestAnimationFrame(update);
+}
+window.requestAnimationFrame(update);
